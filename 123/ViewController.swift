ae099ebf9ed0fa96a4074b9c7a85629b414171dd
TigraTigra <pavelremarque@gmail.com>
2024-01-05 13:20:00 +0700

    Initial Commit

---
+++ 123/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  123
+//
+//  Created by DC on 05.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
